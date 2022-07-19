fd80961978568c48cab785953f843443f7273718
Ted <withwooyong@gmail.com>
2022-07-19 23:38:47 +0900

    Initial commit

---
+++ src/main/java/com/spring/cloud/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,26 @@
+package com.spring.cloud.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+    /**
+     * dev, stg, prd
+     * $ curl -X GET "http://localhost:8080/config/dev"
+     * $ curl -X GET "http://localhost:8080/config/stg"
+     * $ curl -X GET "http://localhost:8080/config/prd"
+     * GET /{application}/{profile}[/{label}]
+     * GET /{application}-{profile}.yml
+     * GET /{label}/{application}-{profile}.yml
+     * GET /{application}-{profile}.properties
+     * GET /{label}/{application}-{profile}.properties
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+
+}
